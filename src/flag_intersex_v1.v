3fc4d2e71b238794a693459ed3a298d4ae79fe47
Rebecca Bettencourt <beckiergb@gmail.com>
2024-09-17 12:52:41 -0700

    Optimize some circles?

--- src/flag_intersex_v1.v
+++ src/flag_intersex_v1.v
@@ -2,10 +2,9 @@
 module flag_intersex_v1 `FLAG;
   wire [9:0] cir_x = (pix_x < 320) ? (320 - pix_x) : (pix_x - 320);
   wire [9:0] cir_y = (pix_y < 240) ? (240 - pix_y) : (pix_y - 240);
-  wire [17:0] cir_r = (cir_x * cir_x) + (cir_y * cir_y);
+  wire [14:0] cir_r = (cir_x * cir_x) + (cir_y * cir_y);
   assign color = (
-    cir_r < 10000 ? `DITHER50(6'b111100,6'b111000) :
-    cir_r < 20000 ? `PURPLE :
+    (cir_x < 144 && cir_y < 144 && cir_r >= 10000 && cir_r < 20000) ? `PURPLE :
     `DITHER50(6'b111100,6'b111000)
   );
 endmodule
--- src/flag_rainbow_progress_2021.v
+++ src/flag_rainbow_progress_2021.v
@@ -3,13 +3,12 @@ module flag_rainbow_progress_2021 `FLAG;
   wire [9:0] tri_y = (pix_y < 240) ? pix_y : (479 - pix_y);
   wire [9:0] cir_x = (pix_x < 70) ? (70 - pix_x) : (pix_x - 70);
   wire [9:0] cir_y = (pix_y < 240) ? (240 - pix_y) : (pix_y - 240);
-  wire [15:0] cir_r = (cir_x * cir_x) + (cir_y * cir_y);
+  wire [12:0] cir_r = (cir_x * cir_x) + (cir_y * cir_y);
   wire [5:0] rainbow_color;
   flag_rainbow_6 rainbow(pix_x, pix_y, rainbow_color);
   assign color = (
     ((pix_x + 60) < tri_y) ? (
-      (cir_r < 2000) ? `YELLOW :
-      (cir_r < 3000) ? `PURPLE :
+      (cir_x < 64 && cir_y < 64 && cir_r >= 2000 && cir_r < 3000) ? `PURPLE :
       `YELLOW
     ) :
     (pix_x < tri_y) ? `WHITE :
