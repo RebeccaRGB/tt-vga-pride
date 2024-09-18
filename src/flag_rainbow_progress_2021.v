9394e0d0cef3365f4f9d48479f84fd8f9662eb6c
Rebecca Bettencourt <beckiergb@gmail.com>
2024-09-17 20:46:23 -0700

    Does this help?

--- src/flag_rainbow_progress_2021.v
+++ src/flag_rainbow_progress_2021.v
@@ -1,8 +1,8 @@
 `include "flag.vh"
 module flag_rainbow_progress_2021 `FLAG;
   wire [9:0] tri_y = (pix_y < 240) ? pix_y : (479 - pix_y);
-  wire [9:0] cir_x = (pix_x < 70) ? (70 - pix_x) : (pix_x - 70);
-  wire [9:0] cir_y = (pix_y < 240) ? (240 - pix_y) : (pix_y - 240);
+  wire [6:0] cir_x = (pix_x < 70) ? (70 - pix_x[6:0]) : (pix_x[6:0] - 70);
+  wire [7:0] cir_y = (pix_y < 240) ? (240 - pix_y[7:0]) : (pix_y[7:0] - 240);
   wire [12:0] cir_r = (cir_x * cir_x) + (cir_y * cir_y);
   wire [5:0] rainbow_color;
   flag_rainbow_6 rainbow(pix_x, pix_y, rainbow_color);
